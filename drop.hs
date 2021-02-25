41afae2b9cdb65e57126aaeccc3f4d4c08843641
MazanYan <mazanyan027@gmail.com>
2021-02-25 22:55:48 +0200

    beforeTheLast function

--- drop.hs
+++ drop.hs
@@ -3,3 +3,10 @@ myDrop n [] = []
 myDrop n xs = if n <= 0
     then xs
     else myDrop (n-1) (tail xs)
+
+beforeTheLast :: [a] -> a
+beforeTheLast []       = error "Cannot get the element before the last"
+beforeTheLast [x]      = error "Cannot get the element before the last"
+beforeTheLast (x : xs) = if length xs == 1
+    then x
+    else beforeTheLast xs
