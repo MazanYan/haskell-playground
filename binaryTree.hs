45925a61ff0aad07faaf5d5d4aaf411f740813ce
MazanYan <mazanyan027@gmail.com>
2021-02-26 13:03:40 +0200

    get height of binary tree

--- binaryTree.hs
+++ binaryTree.hs
@@ -18,3 +18,7 @@ getValue tree (x:xs) = if x == GoLeft
     then getValue (left tree) xs
     else getValue (right tree) xs
 getValue tree [] = Just (value tree)
+
+height :: Tree a -> Int
+height Empty = 0
+height tree  = 1 + max (height (left tree)) (height (right tree))
