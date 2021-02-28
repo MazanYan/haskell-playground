ce8209937cf9fe5f7609457c67d26d7120c435ed
MazanYan <mazanyan027@gmail.com>
2021-02-28 16:06:51 +0200

    implemented several operations over array

--- arrayOperations.hs
+++ arrayOperations.hs
@@ -38,3 +38,33 @@ join (x:xs) sep
     | null xs   = x
     | otherwise = x ++ sep ++ join xs sep
 join [] _       = []
+
+safeHead :: [a] -> Maybe a
+safeHead (x:xs) = Just x
+safeHead []     = Nothing
+
+safeTail :: [a] -> Maybe [a]
+safeTail (x:xs)
+    | not (null xs) = Just xs
+    | otherwise     = Nothing 
+safeTail []         = Nothing 
+
+safeLast :: [a] -> Maybe a
+safeLast arr = safeHead (myReverse arr) 
+
+safeInit :: [a] -> Maybe [a]
+safeInit []             = Nothing
+safeInit arr
+    | length arr == 1   = Nothing
+    | otherwise         = Just (myReverse xs)
+    where (x:xs) = myReverse arr
+
+-- acts similar to 'words' but separates
+-- its input list on every element for which
+-- the predicate returns False
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith _ [] = []
+splitWith pred arr
+    | null rest = [word]
+    | otherwise = (word ++ [head rest]) : splitWith pred (tail rest)
+    where (word, rest) = span pred arr
