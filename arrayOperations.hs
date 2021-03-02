69eee56eae5b98c16dc060dbda84348c7356b16f
MazanYan <mazanyan027@gmail.com>
2021-03-02 17:50:07 +0200

    new basic operations

--- arrayOperations.hs
+++ arrayOperations.hs
@@ -1,4 +1,5 @@
-import Data.List (sortBy)
+import Data.List (sortBy, foldl')
+import Data.Maybe (fromJust, isNothing)
 
 myLength :: [a] -> Int
 myLength (x:xs) = myLength xs + 1
@@ -9,8 +10,7 @@ mySum (x:xs) = mySum xs + x
 mySum []     = 0
 
 myMean :: Fractional a => [a] -> a
-myMean arr = mySum arr / fromIntegral len
-    where len = myLength arr
+myMean arr = mySum arr / fromIntegral (myLength arr)
 
 myReverse :: [a] -> [a]
 myReverse (x:xs) = myReverse xs ++ [x]
@@ -68,3 +68,46 @@ splitWith pred arr
     | null rest = [word]
     | otherwise = (word ++ [head rest]) : splitWith pred (tail rest)
     where (word, rest) = span pred arr
+
+myConcat :: [[a]] -> [a]
+myConcat = foldr concat' []
+    where concat' [] ys = ys
+          concat' x  ys = x ++ ys
+
+takeWhileRecursion :: (a -> Bool) -> [a] -> [a]
+takeWhileRecursion f []     = []
+takeWhileRecursion f (x:xs) = if f x
+    then x : takeWhileRecursion f xs
+    else []
+
+takeWhileFoldR :: (a -> Bool) -> [a] -> [a]
+takeWhileFoldR _ [] = []
+takeWhileFoldR f xs = let (zs, shouldTerminate) = foldr take ([], False) xs
+                      in zs
+    where take x (ys, False) = if f x
+            then (x : ys, False)
+            else (ys, True)
+          take _ (ys, True)  = (ys, True)
+
+myGroupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+myGroupBy f xs = let (zs, old) = foldl' group ([], Nothing) xs
+                 in if isNothing old
+                    then zs
+                    else zs ++ [[fromJust old]]
+    where group (ys, Just old) x = if f old x
+            then (ys ++ [old : [x]], Nothing)
+            else (ys ++ [[old]], Just x)
+          group (ys, Nothing) x = (ys, Just x)
+
+myCycle :: [a] -> [a]
+myCycle (x1:x2:xs) = x1 : myCycle (x2 : xs ++ [x1])
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f = foldr any' False
+    where any' x False = f x
+          any' _ True = True
+
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll f = foldr all' True
+    where all' x True = f x
+          all' _ False = False
